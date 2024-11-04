6c7ce24a1221d3ceaaf2552ad4f82205ee3e4169
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2024-11-04 10:04:12 -0500

    First

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+fun main () {
+    val gameCharacters = mutableListOf(
+        "Mario",
+        "Sonic",
+        "Pac-Man",
+        "Mega Man",
+        "Cloud Strife",
+        "Master Chief",
+        "Doom Guy",
+        "Kratos",
+        "Ray Man",
+        "Crash Bandicoot",
+        "Link",
+        "Mewtwo",
+        "Sora"
+    )
+
+    gameCharacters.add("Steve");
+    gameCharacters[11] = "Ryu";
+    gameCharacters.remove("Mewtwo")
+    println(gameCharacters)
+
+}
